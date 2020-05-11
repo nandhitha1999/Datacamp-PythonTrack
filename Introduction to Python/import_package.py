e4ee4e831d7e36250431b0c0223a903d4a813136
nandhitha1999 <46625586+nandhitha1999@users.noreply.github.com>
2020-05-12 00:12:05 +0530

    import package

---
+++ Introduction to Python/import_package.py
@@ -0,0 +1,17 @@
+# Definition of radius
+r = 0.43
+
+# Import the math package
+
+import math
+# Calculate C
+C = 0
+C = 2*math.pi*r
+
+# Calculate A
+A = 0
+A = math.pi*r**2
+
+# Build printout
+print("Circumference: " + str(C))
+print("Area: " + str(A))
