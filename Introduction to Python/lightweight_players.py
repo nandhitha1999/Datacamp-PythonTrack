95f139e794d75a4c8665626d2a53b70645cd5b3e
nandhitha1999 <46625586+nandhitha1999@users.noreply.github.com>
2020-05-12 01:10:05 +0530

    lightweight baseball players

---
+++ Introduction to Python/lightweight_players.py
@@ -0,0 +1,13 @@
+# Import numpy
+import numpy as np
+
+# Calculate the BMI: bmi
+np_height_m = np.array(height_in) * 0.0254
+np_weight_kg = np.array(weight_lb) * 0.453592
+bmi = np_weight_kg / np_height_m ** 2
+
+# Create the light array
+light = np.array(bmi < 21)
+
+# Print out light
+print(bmi[light])
