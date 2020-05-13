2686f3e748de38683d14ba37e737f261acdd6dc6
nandhitha1999 <46625586+nandhitha1999@users.noreply.github.com>
2020-05-13 13:52:30 +0530

    labels

---
+++ Intermediate Python/labels.py
@@ -0,0 +1,14 @@
+# Strings
+xlab = 'GDP per Capita [in USD]'
+ylab = 'Life Expectancy [in years]'
+title = 'World Development in 2007'
+
+# Add axis labels
+plt.xlabel(xlab)
+plt.ylabel(ylab)
+
+# Add title
+plt.title(title)
+
+# After customizing, display the plot
+plt.show()
