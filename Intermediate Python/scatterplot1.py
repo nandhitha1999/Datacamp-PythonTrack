175a1911bbf1df0744ed9ba078e273e3571da04a
nandhitha1999 <46625586+nandhitha1999@users.noreply.github.com>
2020-05-13 13:43:37 +0530

    scatter plot (2)

--- Intermediate Python/scatterplot1.py
+++ Intermediate Python/scatterplot1.py
@@ -1,7 +1,7 @@
 # Change the line plot below to a scatter plot
 plt.scatter(gdp_cap, life_exp)
 
-# Put the x-axis on a logarithmic scale
+# Put the x-axis on a logarithmic measure
 plt.xscale('log')
 
 # Show plot
