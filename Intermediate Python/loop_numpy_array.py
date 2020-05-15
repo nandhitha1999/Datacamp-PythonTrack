0ac5bf9e2a86348d425c7ba7c385ce58e3993310
nandhitha1999 <46625586+nandhitha1999@users.noreply.github.com>
2020-05-15 18:07:03 +0530

    loop over a numpy array

---
+++ Intermediate Python/loop_numpy_array.py
@@ -0,0 +1,10 @@
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height
+for value in np_height:
+    print(str(value) + " inches")
+
+# For loop over np_baseball
+for value in np.nditer(np_baseball):
+    print(value)
