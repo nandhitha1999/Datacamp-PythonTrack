ef04023a27094544029694be31ad48d619ccdb33
Nandhitha <46625586+nandhitha1999@users.noreply.github.com>
2020-05-17 17:02:45 +0530

    iterating over iterables (1)

---
+++ Python Data Science Toolbox (2)/iterables1.py
@@ -0,0 +1,15 @@
+# Create a list of strings: flash
+flash = ['jay garrick', 'barry allen', 'wally west', 'bart allen']
+
+# Print each list item in flash using a for loop
+for person in flash:
+    print(person)
+
+# Create an iterator for flash: superhero
+superhero = iter(flash)
+
+# Print each item from the iterator
+print(next(superhero))
+print(next(superhero))
+print(next(superhero))
+print(next(superhero))
