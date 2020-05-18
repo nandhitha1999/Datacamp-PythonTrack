1eeeb10656d59e3ad3f5d28392962afdf118a347
Nandhitha <46625586+nandhitha1999@users.noreply.github.com>
2020-05-18 23:35:36 +0530

    functions with one default argument

---
+++ Python DataScience Toolbox (1)/one_default_arg.py
@@ -0,0 +1,23 @@
+# Define shout_echo
+def ____(____, ____):
+    """Concatenate echo copies of word1 and three
+     exclamation marks at the end of the string."""
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = ____
+
+    # Concatenate '!!!' to echo_word: shout_word
+    shout_word = echo_word + '!!!'
+
+    # Return shout_word
+    return shout_word
+
+# Call shout_echo() with "Hey": no_echo
+no_echo = ____
+
+# Call shout_echo() with "Hey" and echo=5: with_echo
+with_echo = ____
+
+# Print no_echo and with_echo
+print(no_echo)
+print(with_echo)
