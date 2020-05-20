a1c03f15b3fbf5977a6ccb620c7fe16e8fcb4d88
Nandhitha <46625586+nandhitha1999@users.noreply.github.com>
2020-05-20 12:29:36 +0530

    writing an iterator to load data in chunks (2)

---
+++ Python Data Science Toolbox (2)/iterator_datachunks2.py
@@ -0,0 +1,14 @@
+# Check out the head of the DataFrame
+print(df_urb_pop.head())
+
+# Check out specific country: df_pop_ceb
+df_pop_ceb = df_urb_pop[df_urb_pop['CountryCode']=='CEB']
+
+# Zip DataFrame columns of interest: pops
+pops = zip(df_pop_ceb['Total Population'],df_pop_ceb['Urban population (% of total)'])
+
+# Turn zip object into list: pops_list
+pops_list = list(pops)
+
+# Print pops_list
+print(pops_list)
